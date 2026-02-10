152155ec039bdaa17c3a1e4b54b02187de0c87d0
Adam Gabriel <adamgabriel289@gmail.com>
2026-02-10 13:45:42 -0400

    Configuração total do projeto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-  output: 'standalone'
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*', // Go roda na porta 3001
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
